4bd03911ec7338d7c923e697fa56b668f3d4de46
Peter Zbinden <peter.zbinden.86@gmail.com>
2024-12-21 16:44:44 +0100

    Initial Commit

---
+++ Source/CollectionExtensions/CollectionExtensions.Tests/ListPositionTests.cs
@@ -0,0 +1,163 @@
+﻿using FluentAssertions;
+
+namespace CollectionExtensions.Tests
+{
+    public class ListPositionTests
+    {
+        [Fact]
+        public void Ensure_IncreaseIndexByOne_MovesItemCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3];
+
+            // Act
+            data.IncreaseIndexByOne(2);
+
+            // Assert
+            data.Should().Equal([1, 3, 2]);
+        }
+
+        [Fact]
+        public void Ensure_IncreaseIndexByOne_DoesNotMoveOutsideOfBounds()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3];
+
+            // Act
+            data.IncreaseIndexByOne(3);
+
+            // Assert
+            data.Should().Equal([1, 2, 3]);
+        }
+
+        [Fact]
+        public void Ensure_ReduceIndexByOne_MovesItemCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3];
+
+            // Act
+            data.ReduceIndexByOne(2);
+
+            // Assert
+            data.Should().Equal([2, 1, 3]);
+        }
+
+        [Fact]
+        public void Ensure_ReduceIndexByOne_DoesNotMoveOutsideOfBounds()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3];
+
+            // Act
+            data.ReduceIndexByOne(1);
+
+            // Assert
+            data.Should().Equal([1, 2, 3]);
+        }
+
+        [Fact]
+        public void Ensure_MoveToStart_MovesItemCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3];
+
+            // Act
+            data.MoveToStart(3);
+
+            // Assert
+            data.Should().Equal([3, 1, 2]);
+        }
+
+        [Fact]
+        public void Ensure_MoveToEnd_MovesItemCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3];
+
+            // Act
+            data.MoveToEnd(1);
+
+            // Assert
+            data.Should().Equal([2, 3, 1]);
+        }
+
+        [Fact]
+        public void Ensure_MoveItem_FromLeftToRightOverMultiplePlaces_MovesOtherItemsCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3, 4, 5, 6];
+
+            // Act
+            data.MoveItem(2, 4);
+
+            // Assert
+            data.Should().Equal([1, 3, 4, 5, 2, 6]);
+        }
+
+        [Fact]
+        public void Ensure_MoveItem_FromRightToLeftOverMultiplePlaces_MovesOtherItemsCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3, 4, 5, 6];
+
+            // Act
+            data.MoveItem(5, 1);
+
+            // Assert
+            data.Should().Equal([1, 5, 2, 3, 4, 6]);
+        }
+
+        [Fact]
+        public void Ensure_MoveItem_DoesNotMoveItemOutOfBounds()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3, 4, 5, 6];
+
+            // Act
+            data.MoveItem(2, 99999);
+
+            // Assert
+            data.Should().Equal([1, 2, 3, 4, 5, 6]);
+        }
+
+        [Fact]
+        public void Ensure_MoveIndex_FromLeftToRightOverMultiplePlaces_MovesOtherItemsCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3, 4, 5, 6];
+
+            // Act
+            data.MoveIndex(1, 4);
+
+            // Assert
+            data.Should().Equal([1, 3, 4, 5, 2, 6]);
+        }
+
+        [Fact]
+        public void Ensure_MoveIndex_FromRightToLeftOverMultiplePlaces_MovesOtherItemsCorrectly()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3, 4, 5, 6];
+
+            // Act
+            data.MoveIndex(4, 1);
+
+            // Assert
+            data.Should().Equal([1, 5, 2, 3, 4, 6]);
+        }
+
+        [Fact]
+        public void Ensure_MoveIndex_DoesNotMoveItemOutOfBounds()
+        {
+            // Arrange
+            List<int> data = [1, 2, 3, 4, 5, 6];
+
+            // Act
+            data.MoveIndex(1, 99999);
+
+            // Assert
+            data.Should().Equal([1, 2, 3, 4, 5, 6]);
+        }
+    }
+}
---
+++ Source/CollectionExtensions/CollectionExtensions/ListExtensions.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CollectionExtensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Moves the specified item one position up in the List
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static bool IncreaseIndexByOne<TItem>(this IList<TItem> list, TItem item)
+        {
+            if (list.Count <= 0)
+            {
+                return false;
+            }
+
+            var index = list.IndexOf(item);
+            if (index < 0)
+            {
+                return false;
+            }
+
+            if (list.Count == 1)
+            {
+                return true;
+            }
+
+            return MoveIndex(list, index, index + 1);
+        }
+
+        /// <summary>
+        /// Moves the specified item one position up in the List
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static bool ReduceIndexByOne<TItem>(this IList<TItem> list, TItem item)
+        {
+            if (list.Count <= 0)
+            {
+                return false;
+            }
+
+            var index = list.IndexOf(item);
+            if (index < 0)
+            {
+                return false;
+            }
+
+            if (list.Count == 1)
+            {
+                return true;
+            }
+
+            return MoveIndex(list, index, index - 1);
+        }
+
+        /// <summary>
+        /// Moves the specified item to the beginning of the list (index 0)
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static bool MoveToStart<TItem>(this IList<TItem> list, TItem item)
+        {
+            if (list.Count <= 0)
+            {
+                return false;
+            }
+
+            var sourceIndex = list.IndexOf(item);
+            if (sourceIndex < 0)
+            {
+                return false;
+            }
+
+            if (list.Count == 1)
+            {
+                return true;
+            }
+
+            var destinationIndex = 0;
+
+            if (destinationIndex >= sourceIndex)
+            {
+                return false;
+            }
+
+            return MoveIndex(list, sourceIndex, destinationIndex);
+        }
+
+        /// <summary>
+        /// Moves the specified item to the end of the list (index n -1)
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static bool MoveToEnd<TItem>(this IList<TItem> list, TItem item)
+        {
+            if (list.Count <= 0)
+            {
+                return false;
+            }
+
+            var sourceIndex = list.IndexOf(item);
+            if (sourceIndex < 0)
+            {
+                return false;
+            }
+
+            if (list.Count == 1)
+            {
+                return true;
+            }
+
+            var destinationIndex = list.Count - 1;
+
+            if (destinationIndex <= sourceIndex)
+            {
+                return false;
+            }
+
+            return MoveIndex(list, sourceIndex, destinationIndex);
+        }
+
+        /// <summary>
+        /// Moves a defined item within the list to another position while ensuring all other items are repositioned
+        /// Does NOT throw Exceptions if index-bounds are violated, instead will return 'false'
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list">The data the operation should be performed on</param>
+        /// <param name="item">Which item should be moved</param>
+        /// <param name="targetIndex">Where should it be moved</param>
+        /// <returns>true if all worked, false if an invalid operation was performed</returns>
+        public static bool MoveItem<TItem>(this IList<TItem> list, TItem item, int targetIndex)
+        {
+            var sourceIndex = list.IndexOf(item);
+            if (sourceIndex < 0)
+            {
+                return false;
+            }
+
+            return MoveIndex(list, sourceIndex, targetIndex);
+        }
+
+        /// <summary>
+        /// Moves a defined item within the list to another position while ensuring all other items are repositioned
+        /// Does NOT throw Exceptions if index-bounds are violated, instead will return 'false'
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list">The data the operation should be performed on</param>
+        /// <param name="sourceIndex">Defines the index of the item that should be moved</param>
+        /// <param name="targetIndex">Where should it be moved</param>
+        /// <returns>true if all worked, false if an invalid operation was performed</returns>
+        public static bool MoveIndex<TItem>(this IList<TItem> list, int sourceIndex, int targetIndex)
+        {
+            if (sourceIndex < 0
+                || targetIndex < 0)
+            {
+                return false;
+            }
+
+            if (sourceIndex >= list.Count
+                || targetIndex >= list.Count)
+            {
+                return false;
+            }
+
+            if (sourceIndex == targetIndex)
+            {
+                return true;
+            }
+
+            MoveUnsafe(list, sourceIndex, targetIndex);
+            return true;
+        }
+
+        /// <summary>
+        /// Moves a defined item within the list to another position while ensuring all other items are repositioned
+        /// No bounds-checks are performed, will throw Exceptions if index-bounds are violated
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="list">The data the operation should be performed on</param>
+        /// <param name="sourceIndex">Which item should be moved</param>
+        /// <param name="targetIndex">Where should it be moved</param>
+        /// <returns>true if all worked, false if an invalid operation was performed</returns>
+        private static void MoveUnsafe<TItem>(this IList<TItem> list, int sourceIndex, int targetIndex)
+        {
+            var temp = list[sourceIndex];
+            if (sourceIndex < targetIndex)
+            {
+                for (int i = sourceIndex; i < targetIndex; i++)
+                {
+                    var source = i + 1;
+                    var destination = i;
+                    list[destination] = list[source];
+                }
+            }
+            else
+            {
+                for (int i = sourceIndex; i > targetIndex; i--)
+                {
+                    var source = i - 1;
+                    var destination = i;
+                    list[destination] = list[source];
+                }
+            }
+
+            list[targetIndex] = temp;
+        }
+
+        public static async Task ForEachAsync<TItem>(this IEnumerable<TItem> list, Func<TItem, Task> action)
+        {
+            foreach (var item in list)
+            {
+                await action(item);
+            }
+        }
+
+        public static void ForEach<TItem>(this IEnumerable<TItem> list, Action<TItem> action)
+        {
+            foreach (var item in list)
+            {
+                action(item);
+            }
+        }
+
+        public static async Task ForEachAsync<TItem>(this IList<TItem> list, Func<TItem, Task> action)
+        {
+            foreach (var item in list)
+            {
+                await action(item);
+            }
+        }
+
+        public static void ForEach<TItem>(this IList<TItem> list, Action<TItem> action)
+        {
+            foreach (var item in list)
+            {
+                action(item);
+            }
+        }
+    }
+}
